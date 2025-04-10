adbfa401a8a38964a93384cb3b4b10c03f72e567
wenqiangleo <1642151528@qq.com>
2025-04-10 14:40:24 +0800

    1

--- .history/docker_build_20250410140151.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# 构建Docker镜像
-docker build -t sandbox-interpreter .
-
-# 运行Docker容器
-docker run -p 5000:5000 -v $(pwd)/logs:/app/logs --name sandbox-interpreter-container sandbox-interpreter
